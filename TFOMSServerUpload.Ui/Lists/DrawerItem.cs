c3b2366d62226e595a6e27a22a4ac1616889c0c1
TiZER1908 <aitakhir1908@gmail.com>
2024-11-04 19:19:57 +0400

    Добавьте файлы проекта.

---
+++ TFOMSServerUpload.Ui/Lists/DrawerItem.cs
@@ -0,0 +1,13 @@
+﻿using Telerik.SvgIcons;
+
+namespace TFOMSServerUpload.Ui.Lists
+{
+    public class DrawerItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public ISvgIcon Icon { get; set; }
+        public static DrawerItem None() => new();
+    }
+}
---
+++ TFOMSServerUpload.Ui/Lists/Files.cs
@@ -0,0 +1,12 @@
+﻿namespace TFOMSServerUpload.Ui.Lists
+{
+    public class Files
+    {
+        public int Id { get; set; }
+        public string Med { get; set; }
+        public string File { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
