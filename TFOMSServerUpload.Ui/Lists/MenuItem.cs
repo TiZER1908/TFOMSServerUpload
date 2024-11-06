b6b4ced00770d2ace53c6a2c5f763d2209fe9223
TiZER1908 <aitakhir1908@gmail.com>
2024-11-06 20:03:26 +0400

    Update

---
+++ TFOMSServerUpload.Ui/Lists/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using Telerik.SvgIcons;
+
+namespace TFOMSServerUpload.Ui.Lists
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public ISvgIcon Icon { get; set; }
+        public List<MenuItem> Items { get; set; }
+    }
+}
